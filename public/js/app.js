38b79b4b70721846877b268e3e50eebbc5aafd01
Rachmar Mohammad <rachmar.dev@gmail.com>
2019-11-05 22:30:02 +0800

    Update

--- public/js/app.js
+++ public/js/app.js
@@ -106795,8 +106795,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Rafael Angelo Castel\Desktop\Dengoy\chedro\chedro\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Rafael Angelo Castel\Desktop\Dengoy\chedro\chedro\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\chedro\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\chedro\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
