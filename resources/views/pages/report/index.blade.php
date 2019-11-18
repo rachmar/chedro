bb07d37da96040b928165601d3ef475936a6e931
rachmarmohammad <rachmar.dev@gmail.com>
2019-11-18 20:42:27 +0800

    Update

---
+++ resources/views/pages/report/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="row">
+
+</div>
+@endsection
+
+@section('script')
+<script type="text/javascript">
+    $( document ).ready(function() {
+      
+    });
+</script>
+@endsection
